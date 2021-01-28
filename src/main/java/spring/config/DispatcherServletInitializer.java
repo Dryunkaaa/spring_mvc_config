ce3347f10acc9fb830bfc0d9b9e5c45ddb17afe8
Andrey <dryunkaaa@gmail.com>
2021-01-28 21:18:18 +0200

    Added mvc annotation config

---
+++ src/main/java/spring/config/DispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MvcWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
